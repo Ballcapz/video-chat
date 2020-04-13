ff4742781245e5e6c963e53c1d32784a9b617404
Zach Johnson <zjohnson@omnitech-inc.com>
2020-04-13 15:27:19 -0500

    Registered socket and resource in main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueSocketIO from 'vue-socket.io'
+import VueResource from 'vue-resource'
+import { url } from './utils/config'
 import router from './router';
 import store from './store';
+
+// Socket config
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: `${url}/video-chat`,
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  },
+}))
+
+
+Vue.use(VueResource)
 
 Vue.config.productionTip = false;
 
